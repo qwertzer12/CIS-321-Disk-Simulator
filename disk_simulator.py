303df9dbc5e147e540a4174607594ab6b949fdc0
qwertzer12 <qwertzer12@outlook.com>
2025-09-11 09:05:44 -0400

    Add Inodes

--- disk_simulator.py
+++ disk_simulator.py
@@ -1,5 +1,11 @@
 import json
 
+class Inode:
+    def __init__(self, file_type: str, size: int, pointers: list):
+        self.file_type = file_type  # 'file' or 'directory'
+        self.size = size            # size in bytes
+        self.pointers = pointers    # list of block indices
+
 drive = [None] * 64
 
 drive[0] = "superblock"
@@ -7,7 +13,7 @@ drive[1] = "inode_bitmap"
 drive[2] = "data_bitmap"
 
 for i in range(3, 8):
-    drive[i] = "inode_" + i.__str__()
+    drive[i] = Inode(file_type=i, size=0, pointers=[]).__dict__
 
 for i in range(8, 64):
     drive[i] = "data_block_" + i.__str__()
@@ -15,7 +21,9 @@ for i in range(8, 64):
 for i in range(len(drive)):
     print(f"Block {i}: {drive[i]}")
 
-json_drive = json.dumps(drive, indent=4)
-with open("virtual_disk.json", "w") as f:
-    f.write(json_drive)
+try:
+    with open("virtual_disk.json", "w") as f:
+        json.dump(drive, f, indent=4)
+except Exception as e:
+    print(f"Error writing to file: {e}")
 
