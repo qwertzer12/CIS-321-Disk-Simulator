522998ab6d34f87a4c1bc74261f79a480e6fb933
qwertzer12 <qwertzer12@outlook.com>
2025-09-11 08:23:36 -0400

    initial

---
+++ main.py
@@ -0,0 +1,26 @@
+import cmd2
+import argparse
+
+class MyApp(cmd2.Cmd):
+    """A simple command-line application using cmd2."""
+    greet_parser = cmd2.Cmd2ArgumentParser()
+    greet_parser.add_argument('-g', '--goodbye', action='store_true', help='switch to say goodbye')
+    greet_parser.add_argument('name', nargs='+', help='name of the person to greet')
+
+    @cmd2.with_argparser(greet_parser)
+    def do_greet(self, args):
+        """Greet the user."""
+        if args.goodbye:
+            self.poutput(f"Goodbye, {' '.join(args.name)}!")
+        else:
+            self.poutput(f"Hello, {args.name}!")
+
+    def do_exit(self, args):
+        """Exit the application."""
+        print("Goodbye!")
+        return True
+
+if __name__ == '__main__':
+    import sys
+    app = MyApp()
+    sys.exit(app.cmdloop())
